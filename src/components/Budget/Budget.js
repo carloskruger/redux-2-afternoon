9bdfd4d6b443df3ec0940b62d6424e144b5f739c
Carlos Kruger <carloskr@yahoo.com>
2020-11-09 17:21:33 -0500

    step5

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -7,14 +7,24 @@ import DisplayPurchases from './../shared/DisplayPurchases';
 import Loading from './../shared/Loading/Loading';
 import Nav from './../shared/Nav';
 import './Budget.css';
+import {connect } from 'react-redux';
+import { requestUserData } from './../../ducks/userReducer'
 
 
 class Budget extends Component {
 
+  componentDidMount() {
+
+      this.props.requestUserData()
+    }
+  
+
   render() {
+    const { loading } = this.props.budget;
+    const { firstName, lastName } = this.props.user;
     return (
       <Background>
-        {true ? <Loading /> : null}
+        {loading ? <Loading /> : null}
         <div className='budget-container'>
           <Nav />
           <div className='content-container'>
@@ -33,4 +43,12 @@ class Budget extends Component {
   }
 }
 
-export default Budget;
+function mapStateToProps(state) {
+  return {
+    budget: state.budget,
+    user: state.user
+  }
+}
+
+
+export default connect(mapStateToProps, { requestUserData } )(Budget);
---
+++ src/ducks/budgetReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+    purchases: [],
+    budgetLimit: null,
+    loading: false
+}
+
+export default function reducer(state, action){
+    state = {...initialState};
+    return state;
+}
